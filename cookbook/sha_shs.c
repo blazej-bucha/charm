8bc75985dbea95f51b4ac6997dd248bdfa7f99c1
Blazej Bucha <blazej.bucha@stuba.sk>
2022-10-19 07:41:16 +0200

    update sha_shs.c to use the new crd structs

--- cookbook/sha_shs.c
+++ cookbook/sha_shs.c
@@ -65,8 +65,8 @@ int main(void)
     /* Compute the Gauss--Legendre grid for a given "nmax" on a sphere that is
      * "1000.0" metres above the reference sphere of spherical harmonic
      * coefficients. */
-    charm_crd *grd = charm_crd_gl(nmax, shcs->r + 1000.0);
-    if (grd == NULL)
+    charm_point *grd_pnt = charm_crd_point_gl(nmax, shcs->r + 1000.0);
+    if (grd_pnt == NULL)
     {
         fprintf(stderr, "Failed to compute the Gauss--Legendre grid.\n");
         exit(CHARM_FAILURE);
@@ -74,7 +74,8 @@ int main(void)
 
 
     /* Allocate the memory for the synthesized signal */
-    double *f = (double *)malloc(grd->nlat * grd->nlon * sizeof(double));
+    double *f = (double *)malloc(grd_pnt->nlat * grd_pnt->nlon *
+                                 sizeof(double));
     if (f == NULL)
     {
         fprintf(stderr, "malloc failure.\n");
@@ -85,7 +86,7 @@ int main(void)
     /* Perform the synthesis.  Since the Gauss--Legendre grid resides "1000.0"
      * metres above the reference sphere of the coefficients, performed is
      * solid spherical harmonic synthesis. */
-    charm_shs_point(grd, shcs, nmax, f, err);
+    charm_shs_point(grd_pnt, shcs, nmax, f, err);
     charm_err_handler(err, 1);
 
 
@@ -93,12 +94,12 @@ int main(void)
     printf("Print some synthesized values of the signal...\n");
     size_t i = 0;
     size_t j = 0;
-    printf("f(%zu, %zu) = %0.16e\n", i, j, f[i * grd->nlon + j]);
+    printf("f(%zu, %zu) = %0.16e\n", i, j, f[i * grd_pnt->nlon + j]);
 
 
-    i = grd->nlat / 2;
-    j = grd->nlon / 2;
-    printf("f(%zu, %zu) = %0.16e\n", i, j, f[i * grd->nlon + j]);
+    i = grd_pnt->nlat / 2;
+    j = grd_pnt->nlon / 2;
+    printf("f(%zu, %zu) = %0.16e\n", i, j, f[i * grd_pnt->nlon + j]);
 
 
     /* Initialize a new structure of spherical harmonic coefficients for
@@ -118,7 +119,7 @@ int main(void)
      * resides on a sphere that is "1000.0" metres above the reference sphere.
      * Therefore, the coefficients are automatically properly rescaled to the
      * desired sphere defined by "shcs2->r". */
-    charm_sha_point(grd, f, nmax, shcs2, err);
+    charm_sha_point(grd_pnt, f, nmax, shcs2, err);
     charm_err_handler(err, 1);
 
 
@@ -152,7 +153,7 @@ int main(void)
 
     /* We will not need some of the structures and arrays anymore, so let's
      * free them. */
-    charm_crd_free(grd);
+    charm_crd_point_free(grd_pnt);
     charm_shc_free(shcs2);
     free(f);
     free(dda);
@@ -172,30 +173,32 @@ int main(void)
     printf("===========================\n");
 
 
-    /* Let's create a "charm_crd" structure to store 3 scattered points. */
-    charm_crd *sctr = charm_crd_init(CHARM_CRD_POINTS_SCATTERED, 3, 3);
-    if (sctr == NULL)
+    /* Let's create a "charm_point" structure to store 3 scattered points. */
+    charm_point *sctr_pnt = charm_crd_point_calloc(CHARM_CRD_POINTS_SCATTERED,
+                                                   3, 3);
+    if (sctr_pnt == NULL)
     {
-        fprintf(stderr, "Failed to initialize the \"charm_crd\" structure.\n");
+        fprintf(stderr,
+                "Failed to initialize the \"charm_point\" structure.\n");
         exit(CHARM_FAILURE);
     }
 
 
-    /* Now let's feed "sctr" with some computation points (angular values must
-     * be provided in radians). */
-    sctr->lat[0] =  0.1;
-    sctr->lat[1] =  0.436231;
-    sctr->lat[2] = -0.9651;
-    sctr->lon[0] =  0.0;
-    sctr->lon[1] =  1.53434;
-    sctr->lon[2] =  4.2316;
-    sctr->r[0]   = shcs->r + 1000.0;
-    sctr->r[1]   = shcs->r + 2000.0;
-    sctr->r[2]   = shcs->r + 3000.0;
+    /* Now let's feed "sctr_pnt" with some computation points (angular values
+     * must be provided in radians). */
+    sctr_pnt->lat[0] =  0.1;
+    sctr_pnt->lat[1] =  0.436231;
+    sctr_pnt->lat[2] = -0.9651;
+    sctr_pnt->lon[0] =  0.0;
+    sctr_pnt->lon[1] =  1.53434;
+    sctr_pnt->lon[2] =  4.2316;
+    sctr_pnt->r[0]   = shcs->r + 1000.0;
+    sctr_pnt->r[1]   = shcs->r + 2000.0;
+    sctr_pnt->r[2]   = shcs->r + 3000.0;
 
 
     /* Allocate memory to store the synthesized signal */
-    f = (double *)malloc(sctr->nlat * sizeof(double));
+    f = (double *)malloc(sctr_pnt->nlat * sizeof(double));
     if (f == NULL)
     {
         fprintf(stderr, "malloc failure.\n");
@@ -204,7 +207,7 @@ int main(void)
 
 
     /* Do the synthesis */
-    charm_shs_point(sctr, shcs, nmax, f, err);
+    charm_shs_point(sctr_pnt, shcs, nmax, f, err);
     charm_err_handler(err, 1);
 
 
@@ -212,11 +215,11 @@ int main(void)
 
 
     /* Print the synthesized values */
-    for (size_t i = 0; i < sctr->nlat; i++)
+    for (size_t i = 0; i < sctr_pnt->nlat; i++)
         printf("%0.16e\n", f[i]);
 
 
-    charm_crd_free(sctr);
+    charm_crd_point_free(sctr_pnt);
     free(f);
 
 
@@ -234,29 +237,30 @@ int main(void)
     printf("===========================\n");
 
 
-    /* Initialize a charm_crd structure to hold a "5 x 10" points grid. */
-    grd = charm_crd_init(CHARM_CRD_POINTS_GRID, 5, 10);
-    if (grd == NULL)
+    /* Initialize a charm_point structure to hold a "5 x 10" point grid. */
+    grd_pnt = charm_crd_point_calloc(CHARM_CRD_POINTS_GRID, 5, 10);
+    if (grd_pnt == NULL)
     {
-        fprintf(stderr, "Failed to initialize the \"charm_crd\" structure.\n");
+        fprintf(stderr,
+                "Failed to initialize the \"charm_point\" structure.\n");
         exit(CHARM_FAILURE);
     }
 
 
     /* Define some grid points */
-    for (size_t i = 0; i < grd->nlat; i++)
+    for (size_t i = 0; i < grd_pnt->nlat; i++)
     {
-        grd->lat[i] = M_PI_2 - ((double)i / (double)grd->nlat) * M_PI;
-        grd->r[i]   = shcs->r + (double)i;
+        grd_pnt->lat[i] = M_PI_2 - ((double)i / (double)grd_pnt->nlat) * M_PI;
+        grd_pnt->r[i]   = shcs->r + (double)i;
     }
-    for (size_t j = 0; j < grd->nlon; j++)
+    for (size_t j = 0; j < grd_pnt->nlon; j++)
     {
-        grd->lon[j] = ((double)j / (double)grd->nlon) * (2.0 * M_PI);
+        grd_pnt->lon[j] = ((double)j / (double)grd_pnt->nlon) * (2.0 * M_PI);
     }
 
 
     /* Initialize an array to store the synthesized signal */
-    f = (double *)malloc(grd->nlat * grd->nlon * sizeof(double));
+    f = (double *)malloc(grd_pnt->nlat * grd_pnt->nlon * sizeof(double));
     if (f == NULL)
     {
         fprintf(stderr, "malloc failure.\n");
@@ -265,17 +269,17 @@ int main(void)
 
 
     /* Do the synthesis */
-    charm_shs_point(grd, shcs, nmax, f, err);
+    charm_shs_point(grd_pnt, shcs, nmax, f, err);
     charm_err_handler(err, 1);
 
 
     /* Print the synthesized values */
-    for (size_t i = 0; i < grd->nlat; i++)
-        for (size_t j = 0; j < grd->nlon; j++)
-            printf("%0.16e\n", f[i * grd->nlon + j]);
+    for (size_t i = 0; i < grd_pnt->nlat; i++)
+        for (size_t j = 0; j < grd_pnt->nlon; j++)
+            printf("%0.16e\n", f[i * grd_pnt->nlon + j]);
 
 
-    charm_crd_free(grd);
+    charm_crd_point_free(grd_pnt);
     free(f);
 
 
@@ -293,9 +297,10 @@ int main(void)
     printf("===========================\n");
 
 
-    /* Initialize a "charm_crd" structure to hold 3 scattered cells. */
-    sctr = charm_crd_init(CHARM_CRD_CELLS_SCATTERED, 3, 3);
-    if (sctr == NULL)
+    /* Initialize a "charm_cell" structure to hold 3 scattered cells. */
+    charm_cell *sctr_cell = charm_crd_cell_calloc(CHARM_CRD_CELLS_SCATTERED,
+                                                  3, 3);
+    if (sctr_cell == NULL)
     {
         fprintf(stderr, "Failed to initialize scattered cells.\n");
         exit(CHARM_FAILURE);
@@ -305,40 +310,40 @@ int main(void)
     /* Define more or less randomly some scattered cells */
     /* --------------------------------------------------------------------- */
     /* The maximum latitude of the first cell */
-    sctr->lat[0] =  0.323413435;
+    sctr_cell->latmax[0] =  0.323413435;
 
     /* The minimum latitude of the first cell */
-    sctr->lat[1] =  sctr->lat[0] - 0.234323;
+    sctr_cell->latmin[0] =  sctr_cell->latmax[0] - 0.234323;
 
     /* The maximum latitude of the second cell */
-    sctr->lat[2] = -0.90234320952;
+    sctr_cell->latmax[1] = -0.90234320952;
 
     /* The minimum latitude of the second cell */
-    sctr->lat[3] =  sctr->lat[2] - 0.4456;
+    sctr_cell->latmin[1] =  sctr_cell->latmax[1] - 0.4456;
 
     /* And so on... */
-    sctr->lat[4] =  0.0;
-    sctr->lat[5] =  sctr->lat[4] - M_PI_2;
+    sctr_cell->latmax[2] =  0.0;
+    sctr_cell->latmin[2] =  sctr_cell->latmax[2] - M_PI_2;
 
     /* And now the same, but with the longitudes... */
-    sctr->lon[0] =  0.123456789;
-    sctr->lon[1] =  sctr->lon[0] + 1.3235;
-    sctr->lon[2] =  4.3445234;
-    sctr->lon[3] =  sctr->lon[2] + 0.01;
-    sctr->lon[4] =  0.0;
-    sctr->lon[5] =  sctr->lon[4] + 2.0 * M_PI;
+    sctr_cell->lonmin[0] =  0.123456789;
+    sctr_cell->lonmax[0] =  sctr_cell->lonmin[0] + 1.3235;
+    sctr_cell->lonmin[1] =  4.3445234;
+    sctr_cell->lonmax[1] =  sctr_cell->lonmin[1] + 0.01;
+    sctr_cell->lonmin[2] =  0.0;
+    sctr_cell->lonmax[2] =  sctr_cell->lonmin[2] + 2.0 * M_PI;
 
     /* Finally, we define spherical radii of the scattered cells.  Importantly,
      * the spherical radius is *constants* over the cells (but may vary from
      * cell to cell). */
-    sctr->r[0]   =  shcs->r;
-    sctr->r[1]   =  shcs->r + 1000.0;
-    sctr->r[2]   =  shcs->r + 2000.0;
+    sctr_cell->r[0]      =  shcs->r;
+    sctr_cell->r[1]      =  shcs->r + 1000.0;
+    sctr_cell->r[2]      =  shcs->r + 2000.0;
     /* --------------------------------------------------------------------- */
 
 
     /* Initialize an array to store the synthesized signal */
-    f = (double *)malloc(grd->nlat * sizeof(double));
+    f = (double *)malloc(sctr_cell->nlat * sizeof(double));
     if (f == NULL)
     {
         fprintf(stderr, "malloc failure.\n");
@@ -347,16 +352,16 @@ int main(void)
 
 
     /* Do the synthesis */
-    charm_shs_cell(sctr, shcs, nmax, f, err);
+    charm_shs_cell(sctr_cell, shcs, nmax, f, err);
     charm_err_handler(err, 1);
 
 
     /* Print the synthesized values */
-    for (size_t i = 0; i < sctr->nlat; i++)
+    for (size_t i = 0; i < sctr_cell->nlat; i++)
         printf("%0.16e\n", f[i]);
 
 
-    charm_crd_free(sctr);
+    charm_crd_cell_free(sctr_cell);
     free(f);
 
 
@@ -375,40 +380,43 @@ int main(void)
     printf("===========================\n");
 
 
-    /* Initialize a "charm_crd" structure to hold a grid of cells with "15"
+    /* Initialize a "charm_cell" structure to hold a grid of cells with "15"
      * cells in the latitudinal direction and "30" cells in the longitudinal
      * direction. */
-    grd = charm_crd_init(CHARM_CRD_CELLS_GRID, 15, 30);
+    charm_cell *grd_cell = charm_crd_cell_calloc(CHARM_CRD_CELLS_GRID, 15, 30);
 
 
     /* Define some grid cells */
-    for (size_t i = 0; i < grd->nlat; i++)
+    for (size_t i = 0; i < grd_cell->nlat; i++)
     {
-        /* The minimum cell latitudes */
-        grd->lat[2 * i]      = M_PI_2 - ((double)i / (double)grd->nlat) * M_PI;
-
         /* The maximum cell latitudes */
-        grd->lat[2 * i + 1]  = M_PI_2 - ((double)(i + 1) / (double)grd->nlat) *
-                                        M_PI;
+        grd_cell->latmax[i] = M_PI_2 -
+                              ((double)i / (double)grd_cell->nlat) * M_PI;
+
+        /* The minimum cell latitudes */
+        grd_cell->latmin[i] = M_PI_2 -
+                              ((double)(i + 1) / (double)grd_cell->nlat) *
+                              M_PI;
 
         /* The spherical radii (may vary with the latitude, but we use
          * a constant value here, because of the example that follows after
          * this one). */
-        grd->r[i] = shcs->r + 1000.0;
+        grd_cell->r[i] = shcs->r + 1000.0;
     }
-    for (size_t j = 0; j < grd->nlon; j++)
+    for (size_t j = 0; j < grd_cell->nlon; j++)
     {
         /* The minimum cell longitudes */
-        grd->lon[2 * j]      = ((double)j / (double)grd->nlon) * (2.0 * M_PI);
+        grd_cell->lonmin[j] = ((double)j / (double)grd_cell->nlon) *
+                              (2.0 * M_PI);
 
         /* The maximum cell longitudes */
-        grd->lon[2 * j + 1]  = ((double)(j + 1) / (double)grd->nlon) * 
-                               (2.0 * M_PI);
+        grd_cell->lonmax[j] = ((double)(j + 1) / (double)grd_cell->nlon) *
+                              (2.0 * M_PI);
     }
 
 
     /* Initialize an array to store the synthesized signal */
-    f = (double *)malloc(grd->nlat * grd->nlon * sizeof(double));
+    f = (double *)malloc(grd_cell->nlat * grd_cell->nlon * sizeof(double));
     if (f == NULL)
     {
         fprintf(stderr, "malloc failure.\n");
@@ -417,7 +425,7 @@ int main(void)
 
 
     /* Do the synthesis */
-    charm_shs_cell(grd, shcs, nmax, f, err);
+    charm_shs_cell(grd_cell, shcs, nmax, f, err);
     charm_err_handler(err, 1);
 
 
@@ -425,12 +433,12 @@ int main(void)
     /* --------------------------------------------------------------------- */
     i = 0;
     j = 10;
-    printf("f(%zu, %zu) = %0.16e\n", i, j, f[i * grd->nlon + j]);
+    printf("f(%zu, %zu) = %0.16e\n", i, j, f[i * grd_cell->nlon + j]);
 
 
     i = 4;
     j = 3;
-    printf("f(%zu, %zu) = %0.16e\n", i, j, f[i * grd->nlon + j]);
+    printf("f(%zu, %zu) = %0.16e\n", i, j, f[i * grd_cell->nlon + j]);
     /* --------------------------------------------------------------------- */
 
 
@@ -448,8 +456,8 @@ int main(void)
     printf("===========================\n");
 
 
-    /* In this example, we use the "grd" structure and the signal "f" from the
-     * previous example. */
+    /* In this example, we use the "grd_cell" structure and the signal "f" from
+     * the previous example. */
 
 
     /* Initialize a new structure of spherical harmonic coefficients for
@@ -464,7 +472,7 @@ int main(void)
 
 
     /* Do the analysis using the method of the approximate quadrature */
-    charm_sha_cell(grd, f, nmax, CHARM_SHA_CELL_AQ, shcs2, err);
+    charm_sha_cell(grd_cell, f, nmax, CHARM_SHA_CELL_AQ, shcs2, err);
     charm_err_handler(err, 1);
 
 
@@ -497,7 +505,7 @@ int main(void)
     /* --------------------------------------------------------------------- */
 
 
-    charm_crd_free(grd);
+    charm_crd_cell_free(grd_cell);
     charm_shc_free(shcs2);
     free(f);
 
