8c5967495a9affafae62d75e6ba0de5346fbb9e5
Blazej Bucha <blazej.bucha@stuba.sk>
2024-04-29 11:44:44 +0200

    remove some if statements that are always true

--- src/err/err_set.c
+++ src/err/err_set.c
@@ -28,18 +28,14 @@ void CHARM(err_set)(CHARM(err) *err,
     unsigned int curr_level = err->level;
 
 
-    /* Write the file name */
-    if (CHARM_ERR_MAX_FILE > 0)
-    {
-        /* If the length of the string in "file" is longer than
-         * "CHARM_ERR_MAX_FILE" characters, some information will be lost.  But
-         * at least a buffer overflow should never happen.  At any rate,
-         * "CHARM_ERR_MAX_FILE" is large enough, so we should really be on the
-         * safe side in any reasonable circumstances.  The same applies to
-         * other parts of this function. */
-        strncpy(err->file[curr_level], file, CHARM_ERR_MAX_FILE - 1);
-        err->file[curr_level][CHARM_ERR_MAX_FILE - 1] = '\0';
-    }
+    /* Write the file name.  If the length of the string in "file" is longer
+     * than "CHARM_ERR_MAX_FILE" characters, some information will be lost.
+     * But at least a buffer overflow should never happen.  At any rate,
+     * "CHARM_ERR_MAX_FILE" is large enough, so we should really be on the safe
+     * side in any reasonable circumstances.  The same applies to other parts
+     * of this function. */
+    strncpy(err->file[curr_level], file, CHARM_ERR_MAX_FILE - 1);
+    err->file[curr_level][CHARM_ERR_MAX_FILE - 1] = '\0';
 
 
     /* Write the line number */
@@ -47,11 +43,8 @@ void CHARM(err_set)(CHARM(err) *err,
 
 
     /* Write the function name */
-    if (CHARM_ERR_MAX_FUNC > 0)
-    {
-        strncpy(err->func[curr_level], func, CHARM_ERR_MAX_FUNC - 1);
-        err->func[curr_level][CHARM_ERR_MAX_FUNC - 1] = '\0';
-    }
+    strncpy(err->func[curr_level], func, CHARM_ERR_MAX_FUNC - 1);
+    err->func[curr_level][CHARM_ERR_MAX_FUNC - 1] = '\0';
 
 
     /* Write the error code */
@@ -59,11 +52,8 @@ void CHARM(err_set)(CHARM(err) *err,
 
 
     /* Write the error message */
-    if (CHARM_ERR_MAX_MSG > 0)
-    {
-        strncpy(err->msg, msg, CHARM_ERR_MAX_MSG - 1);
-        err->msg[CHARM_ERR_MAX_MSG - 1] = '\0';
-    }
+    strncpy(err->msg, msg, CHARM_ERR_MAX_MSG - 1);
+    err->msg[CHARM_ERR_MAX_MSG - 1] = '\0';
 
 
     /* Finally, increase "err->level" by "1", since we have just written some
