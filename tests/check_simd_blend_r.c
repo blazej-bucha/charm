1408f19ac24b4cb2cb8abbd5ad88361c201a4f6a
Blazej Bucha <blazej.bucha@stuba.sk>
2024-08-02 10:50:42 +0200

    fix newly introduced BLEND_R test for AVX-512

--- tests/check_simd_blend_r.c
+++ tests/check_simd_blend_r.c
@@ -4,9 +4,8 @@
 #include <stdio.h>
 #include "../src/prec.h"
 #include "../src/simd/simd.h"
-#include "../src/misc/misc_is_nearly_equal.h"
-#include "check_simd_blend_r.h"
 #include "cmp_arrays.h"
+#include "check_simd_blend_r.h"
 /* ------------------------------------------------------------------------- */
 
 
@@ -36,20 +35,6 @@
 
 
 
-/* ------------------------------------------------------------------------- */
-#if CHARM_FLOAT
-#   define TRUE_BITS 0xFFFFFFFF
-#else
-#   define TRUE_BITS 0xFFFFFFFFFFFFFFFFLL
-#endif
-union ir { INT i; REAL r;};
-/* ------------------------------------------------------------------------- */
-
-
-
-
-
-
 /* Check the "BLEND_R" macro from "../src/simd/simd.h".  If compiled without
  * the SIMD instructions, the function returns zero and is not called from the
  * main check program.  To make the program independent, it intentionally does
@@ -63,16 +48,20 @@ long int check_simd_blend_r(void)
     long int e = 0;
 
 
-    const REAL TRUE_VAL  = PREC(9999.0);
-    const REAL FALSE_VAL = PREC(-9999.0);
+    const int TRUE_VAL_int  =  9999;
+    const int FALSE_VAL_int = -9999;
 
 
-    const REAL_SIMD TRUE  = SET1_R(TRUE_VAL);
-    const REAL_SIMD FALSE = SET1_R(FALSE_VAL);
+    const REAL TRUE_VAL_REAL  = (REAL)TRUE_VAL_int;
+    const REAL FALSE_VAL_REAL = (REAL)FALSE_VAL_int;
 
 
-    const REAL_SIMD ZERO = SET_ZERO_RI;
-    const REAL_SIMD ONE  = SET1_RI(1);
+    const REAL_SIMD TRUE  = SET1_R(TRUE_VAL_REAL);
+    const REAL_SIMD FALSE = SET1_R(FALSE_VAL_REAL);
+
+
+    const REAL_SIMD ZERO = SET_ZERO_R;
+    const REAL_SIMD ONE  = SET1_R(PREC(1.0));
 
 
     REAL_SIMD xv = BLEND_R(FALSE, TRUE, EQ_R(ZERO, ZERO));  /* "mask" is all
@@ -83,7 +72,7 @@ long int check_simd_blend_r(void)
 
     REAL xref[SIMD_SIZE];
     for (size_t i = 0; i < SIMD_SIZE; i++)
-        xref[i] = TRUE_VAL;
+        xref[i] = TRUE_VAL_REAL;
 
 
     if (cmp_arrays(x, xref, SIMD_SIZE, CHARM(glob_threshold)))
@@ -98,7 +87,7 @@ long int check_simd_blend_r(void)
 
 
     for (size_t i = 0; i < SIMD_SIZE; i++)
-        xref[i] = FALSE_VAL;
+        xref[i] = FALSE_VAL_REAL;
 
 
     if (cmp_arrays(x, xref, SIMD_SIZE, CHARM(glob_threshold)))
@@ -108,29 +97,18 @@ long int check_simd_blend_r(void)
     }
 
 
-    REAL_SIMD maskv;
-    REAL mask[SIMD_SIZE];
-    union ir true_bits;  true_bits.i  = TRUE_BITS;
-    union ir false_bits; false_bits.i = 0;
     for (size_t j = 0; j < SIMD_SIZE - 1; j++)
     {
         size_t k;
         for (k = 0; k <= j; k++)
-        {
-            mask[k] = false_bits.r;
-            xref[k] = FALSE_VAL;
-        }
+            xref[k] = FALSE_VAL_REAL;
 
 
         for (; k < SIMD_SIZE; k++)
-        {
-            mask[k] = true_bits.r;
-            xref[k] = TRUE_VAL;
-        }
+            xref[k] = TRUE_VAL_REAL;
 
 
-        maskv = LOAD_R(mask);
-        xv    = BLEND_R(FALSE, TRUE, maskv);
+        xv = BLEND_R(FALSE, TRUE, EQ_R(TRUE, LOAD_R(xref)));
         STORE_R(&x[0], xv);
 
 
